65e9bf7087267a6b2d05df537ae819bcf0f3229a
Renan Coutinho <renan.coutinho.11@hotmail.com>
2025-05-30 13:05:10 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploPOO/Models/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    internal class ContaCorrente
+    {
+
+        public ContaCorrente(int NumeroConta, decimal saldoInicial)
+        {
+
+            NumeroConta = numeroConta;
+            saldoConta = saldoInicial;
+
+        }
+        public int numeroConta { get; set; }
+
+        private decimal saldoConta;
+
+        public void sacarValor(decimal valor)
+        {
+            if (saldoConta >= valor)
+            {
+                saldoConta = valor;
+                Console.WriteLine($"Saque no valor de {valor.ToString("C")} realizado com sucesso!");
+
+            }
+            else
+            {
+                Console.WriteLine($"Não foi possivel realizar o saque de {valor.ToString("C")}, valor maior que o saldo de {saldoConta.ToString("C")} disponivel !");
+            }
+        }
+
+        public void exibirSaldo()
+        {
+            Console.WriteLine($"Saldo Disponivel: {saldoConta.ToString("C")}");
+
+        }
+
+
+
+    }
+}
---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ExemploPOO.Models;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.Nome = "Renan";
+pessoa.Idade = 30;
+
+pessoa.Apresentar();
+
+
+
+ContaCorrente c1 = new ContaCorrente(200, 1000);
+
+c1.exibirSaldo();
+c1.sacarValor(600);
+c1.exibirSaldo();
+
+
+
