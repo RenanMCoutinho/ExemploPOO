bbbc55c9a9392a1cb1d065ddd3e176c5b4ae37f2
Renan Coutinho <renan.coutinho.11@hotmail.com>
2025-05-30 21:44:30 -0300

    update3

--- ExemploPOO/Models/Professor.cs
+++ ExemploPOO/Models/Professor.cs
@@ -9,12 +9,13 @@ namespace ExemploPOO.Models
     internal class Professor : Pessoa
     {
         public decimal salario { get; set; }
-        
+
         public override void Apresentar()
         {
             //base.Apresentar();
             Console.WriteLine($"Olá meu nome é {Nome}, tenho {Idade} anos e meu salário é {salario:C}!");
         }
+
 
 
     }
